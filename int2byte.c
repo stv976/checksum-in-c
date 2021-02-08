07e8f591d90f2530bf6e7093cc058cfd0c5d7f14
Steven Fitzgerald <steve@csun.edu>
2021-02-08 08:54:49 -0800

    Initial Version

---
+++ int2byte.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define max_int (255)
+#define byte char
+
+
+int main (int argc, char * argv[], char ** envp) {
+
+   int input;
+   byte value;
+   while  ( ! feof(stdin) ) {
+     scanf("%d", &input);
+     if (input > max_int) {
+       fprintf(stderr, "Error in input!\n");
+       exit(1);
+     }
+     value = (byte) input;
+     write(1, &value, sizeof(byte));
+  }
+}
